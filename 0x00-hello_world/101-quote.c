36bc84f3252116ef2882a2e895c706508475a6e2
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-03 19:28:17 +0000

    to write nbytes from buffer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-// print to stderr
-// write() function to write nbytes from buffer
-
+/*
+* main - iprint to stderr
+* write() function to write nbytes from buffer
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-int main(void){
-
-   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-   return (1);
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
