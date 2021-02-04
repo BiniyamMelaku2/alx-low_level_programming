4f064714ce8faba169121df5f88345617bed4b18
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-04 13:38:39 +0000

    prints all single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers
+* Return: 0 no Error
+*/
+int main(void)
+{
+int ch = 48;
+while (ch < 58)
+{
+putchar(ch);
+ch++;
+}
+putchar(10);
+return (0);
+}
