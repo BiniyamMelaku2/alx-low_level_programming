28450339819bc6c64e12601c875026e842171c1d
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-09 18:07:36 +0000

    prints first 50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints 50 fibonacci numbers
+*
+* Return: 0 No Erro
+*/
+
+int main(void)
+{
+long prev, next, temp;
+int counter;
+prev = 1;
+next = 2;
+counter = 1;
+while (counter < 50)
+{
+printf("%li, ", prev);
+temp = prev + next;
+prev = next;
+next = temp;
+counter++;
+}
+printf("%li\n", next);
+return (0);
+}
