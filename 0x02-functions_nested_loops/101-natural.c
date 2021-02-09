60fd5cfb7772f0567fa1f14ab7ad16462eb207a8
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-09 18:01:41 +0000

    sum of multiples 3,5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints sum of multiples of 3, 5
+* Return: 0 NoError
+*/
+
+int main(void)
+{
+int count, sum;
+for (count = 1; count <= 1024 ; count++)
+{
+if (count % 3 == 0)
+{
+if (count % 5 == 0)
+{
+sum += count;
+}
+}
+}
+printf("%d\n", sum);
+return (0);
+}
