8374912d9e547ec799fe059cac992ff7d7222867
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-10 17:52:26 +0000

    prints the first 98 Fibonacc

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
 * main - prints 98 fibonacci numbers
 *
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-long double prev, next, temp;
+long prev, next, temp;
 int counter;
 prev = 1;
 next = 2;
 counter = 1;
-while (counter < 98)
+while (counter< 85)
 {
-printf("%0.Lf, ", prev);
+printf("%li, ", prev);
 temp = prev + next;
 prev = next;
 next = temp;
 counter++;
 }
-printf("%0.Lf\n", next);
+printf("%li\n", prev);
 return (0);
 }
