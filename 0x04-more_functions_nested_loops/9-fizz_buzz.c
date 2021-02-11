6b9a3ffb14f52d21c45280fad05a0d3258879a36
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-11 08:23:39 +0000

    The Fizz-Buzz Test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Fizz-Buzz test 1-100
+* Return: 0 NoError
+*/
+
+int main(void)
+{
+int num  = 1;
+while (num < 101)
+{
+if ((num % 3 == 0) &&  (num % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (num % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (num % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", num);
+}
+num++;
+}
+printf("\n");
+return (0);
+}
+
