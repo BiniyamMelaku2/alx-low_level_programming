2ebce7e7555d02bfaf59bcf4167b1bab276ae72b
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-11 01:03:41 +0000

    prints a square #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_square - prints a square
+* @size: argument passes sqaure size
+* Return: 0 no Error
+*/
+void print_square(int size)
+{
+int count, width;
+
+for (width = 1; width <= size ; width++)
+{
+count = size;
+while (count)
+{
+_putchar(35);
+count--;
+}
+_putchar('\n');
+}
+}
