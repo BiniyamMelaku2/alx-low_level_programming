8780bf41f0fd2a7f6cf0d3c8ac01a3159b06defd
BiniyamMelaku2 <biniyammelaku2@gmail.com>
2021-02-24 18:06:50 +0000

    compares two strings

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,20 +14,17 @@ if (!*s1 && !*s2)
 {
 return (1);
 }
-else if (*s1 == *s2)
+if (*s1 == *s2)
 {
 return (wildcmp(s1 + 1, s2 + 1));
 }
-else if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 {
 return (1);
 }
-else if (*s2 == '*' && *(s1 + 1) != '\0' && !*s2)
+if (*s2 == '*' && *(s1 + 1) != '\0' && !*s2)
 {
 return (0);
 }
-else
-{
 return (0);
-}
 }
