c4d9ff6ccd565a7c7d823e1670b0772e0d7c124f
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-27 13:34:20 +0000

    containing all prototype function

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,9 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
-int _abs(int);
 int _islower(int c);
 int _isalpha(int c);
+int _abs(int);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
