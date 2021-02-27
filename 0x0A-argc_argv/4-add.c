560a28d0b952eb05abaaeb6c2f2512eb3d5bba7f
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-27 15:52:27 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program adds number of arguments
+* @argc: size of argv array
+* @argv: an array of command line argument
+* Return: 0 NoError
+*/
+
+int main(int argc, char *argv[])
+{
+int c, num = 0;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+for (c = 1; c < argc; c++)
+{
+if (!atoi(argv[c]))
+{
+printf("Error\n");
+return (1);
+}
+}
+
+for (c = 0; c < argc; c++)
+{
+num += atoi(argv[c]);
+}
+printf("%d\n", num);
+return (0);
+}
