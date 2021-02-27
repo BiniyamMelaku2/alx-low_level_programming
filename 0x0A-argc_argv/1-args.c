1cc1a88fd0650889c4f76ec61e95799f445fb799
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-02-27 15:50:35 +0000

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - program entry point
+* @argc: size of argv array
+* @argv: an array of command line argument
+* Return: 0 NoError
+*/
+
+int main(int argc, char *argv[])
+{
+if (argv)
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+}
