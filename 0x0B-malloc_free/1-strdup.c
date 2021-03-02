91844543115739fdbc8c89ad6e26b520c8947953
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-03-02 15:50:51 +0000

    returns a pointer to allocated space

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies string returns pointer
+ * @str: string value to copy
+ * Return: char pointer to memory
+ */
+
+char *_strdup(char *str)
+{
+char *ptr = str;
+unsigned int index = 0, lenstr = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+else
+{
+for (lenstr = 0 ; str[lenstr]; lenstr++)
+;
+ptr = malloc(sizeof(char) * lenstr);
+if (ptr == NULL)
+{
+return (NULL);
+}
+else
+{
+while (index < lenstr)
+{
+ptr[index] = str[index];
+index++;
+}
+}
+}
+return (ptr);
+}
