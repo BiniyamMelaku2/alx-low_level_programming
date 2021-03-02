714a1a7d19440009791fa1095c03cb54695941f0
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-03-02 15:51:38 +0000

    concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - check the code for Holberton School students.
+ * @s1: holds value for string1
+ * @s2: holds value for string2
+ * Return: pointer to stored string
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *ptr;
+unsigned int index = 0, lens1 = 0, lens2 = 0;
+if (s1 == NULL)
+ptr = "";
+for (lens1 = 0 ; s1[lens1]; lens1++)
+;
+for (lens2 = 0 ; s2[lens2]; lens2++)
+;
+ptr = malloc(sizeof(char) * (lens1 + lens2));
+if (ptr == NULL)
+return (NULL);
+while (*s1)
+{
+ptr[index] = *s1++;
+index++;
+}
+while (*s2)
+{
+ptr[index] = *s2++;
+index++;
+}
+ptr[index] = '\0';
+
+return (ptr);
+}
