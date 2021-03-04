26a2f671a50dfcdd75a3db9ae12d718986c0508d
Biniyam Melaku <biniyammelaku2@gmail.com>
2021-03-04 15:47:32 +0000

    allocates memory for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,15 +13,25 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int ct;
 char *ptr;
-ptr = malloc(sizeof(size) * nmemb * size);
 
 if (nmemb == 0 || size == 0)
+{
 return (NULL);
+}
+else
+{
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
+{
 return (NULL);
-
-for (ct = 0; ct < (sizeof(size) * nmemb * size); ct++)
-ptr[ct] = 0;
-
-return ((void *)ptr);
+}
+else
+{
+for (ct = 0; ct < (nmemb * size); ct++)
+{
+*(ptr + ct) = 0;
+}
+}
+}
+return (ptr);
 }
