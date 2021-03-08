461f207d132efda9a073f1367b82112cd19984ff
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-08 18:24:03 +0000

    includes all function prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
 #ifndef MACRO_STRUCT_DOG_H
 #define MACRO_STRUCT_DOG_H
-#define dog_t struct dog
+#include "dog.h"
 /**
  * struct dog - struct type definition
  * @name: name type char member
  * @age: age type float member
  * @owner: owner type char member
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
@@ -16,6 +16,7 @@ char *owner;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
