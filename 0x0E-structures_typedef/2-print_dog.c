01e9018378777f089b96c5acf77158e5f7c10db0
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-08 18:09:14 +0000

    prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code for Holberton School students.
+ * @d: pointer to struct type dog
+ */
+ 
+void print_dog(struct dog *d)
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
