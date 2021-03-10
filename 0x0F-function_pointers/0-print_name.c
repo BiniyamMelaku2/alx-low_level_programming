a81af4ca5e2d57ed4c9b5ca61e86c139bb99824a
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-10 19:10:20 +0000

    run function over array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: pointer to the name string
@@ -7,5 +7,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
+{
 (*f)(name);
+}
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * array_iterator - prints every element of the array
  * @array: pointer to the array
@@ -9,6 +10,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t c;
+if (array != NULL && action != NULL)
+{
 for (c = 0; c < size; c++)
+{
 action(array[c]);
+}
+}
 }
