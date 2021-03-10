c58f840b30098268bb1e8d8039a8b0f938e48776
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-10 19:15:45 +0000

    searches for an integer.

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,8 +11,17 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int c;
+if (size <= 0)
+{
+return (-1);
+}
+if (array != NULL && cmp != NULL)
+{
 for (c = 0; c < size; c++)
+{
 if (cmp(array[c]))
 return (c);
+}
+}
 return (-1);
 }
