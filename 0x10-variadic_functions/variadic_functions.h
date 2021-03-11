6007b8a8214e4664c48cbdd5b449382dc08e2d4f
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-11 11:02:10 +0000

    prototypes of all functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,28 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct struct_formats - definition for format types
+ * @ch: int, float, char or string type
+ * @func: format function
+ * opt_formats: type definition for a new type
+ */
+typedef struct struct_formats
+{
+char *ch;
+void (*func)(va_list);
+} opt_formats;
+
+void print_char(va_list valst);
+void print_int(va_list valst);
+void print_float(va_list valst);
+void print_string(va_list valst);
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
