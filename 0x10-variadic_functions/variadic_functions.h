4e1d88a508fd6ce25bb75f27a57c696f5fb3ded6
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-11 12:18:45 +0000

    includes all prototype def

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,10 +15,10 @@ char *ch;
 void (*func)(va_list);
 } opt_formats;
 
-void print_char(va_list valst);
-void print_int(va_list valst);
-void print_float(va_list valst);
-void print_string(va_list valst);
+void print_char(va_list va_arg_lst);
+void print_int(va_list va_arg_lst);
+void print_float(va_list va_arg_lst);
+void print_string(va_list va_arg_lst);
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
