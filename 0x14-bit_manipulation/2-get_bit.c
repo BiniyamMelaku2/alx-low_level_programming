024b25c3a5c78abc01e26ff2bf9fc795418ba303
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-03-25 13:07:44 +0000

    returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,16 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned long int num = n;
 unsigned int count = index;
 for (count = 0; num; count++)
+{
 num >>= 1;
-if (index >= count)
+}
+if (index > 64)
+{
 return (-1);
+}
 for (count = index; count; count--)
+{
 n >>= 1;
+}
 return (n & 1);
 }
