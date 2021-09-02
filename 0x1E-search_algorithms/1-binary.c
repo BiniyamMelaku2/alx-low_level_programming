3a020e965bc5020f3f198201cb5ec77325590621
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-09-02 02:42:29 +0000

    searches for a value by Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,22 @@
 #include "search_algos.h"
 
+
+/**
+ * display - prints an array to the console
+ * @array: a pointer to the first element of the array
+ * @low: low index of array
+ * @high: high index of array
+ */
+
+void display(int *array, size_t low, size_t high)
+{
+size_t c;
+printf("Searching in array: ");
+for (c = low; c < high; c++)
+printf("%d, ", array[c]);
+printf("%d\n", array[c]);
+}
+
 /**
  * binary_search - searches for a value by Binary search algorithm
  * @array: a pointer to the first element of the array
@@ -10,39 +27,20 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-size_t c = 0, pivot = 0, index = 0, loop = size;
+size_t pivot = 0, index = 0, high = 0;
 if (size == 0 || !array)
 return (-1);
-while (loop--)
+high = size - 1;
+while (high >= index)
 {
-printf("Searching in array: ");
-for (c = 0; c < size - 1; c++)
-printf("%d, ", array[c]);
-printf("%d\n", array[c]);
-pivot = (size + size % 2) / 2;
-if (value == array[pivot - 1])
-{
-return (pivot - 1 + index);
-}
-if (pivot < 2)
-{
-printf("Searching in array: ");
-printf("%d\n", array[pivot]);
+display(array, index, high);
+pivot = (high + index) / 2;
 if (value == array[pivot])
-return (pivot + index);
-break;
-}
-else if (value < array[pivot - 1])
-{
-size = pivot - 1;
-continue;
-}
-else if (value > array[pivot - 1])
-{
-array += pivot;
-index = size / 2 + size % 2;
-size = size - pivot;
-}
+return (pivot);
+else if (value < array[pivot])
+high = pivot - 1;
+else
+index = pivot + 1;
 }
 return (-1);
 }
